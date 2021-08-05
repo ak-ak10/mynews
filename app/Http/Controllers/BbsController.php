3c551ceb476a5235dd49b1dec6ff5fd397a89bcc
名前 <>
2021-08-05 07:07:51 +0000

    マイグレーションファイルからpostsテーブルを作成し、フォームから保存

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class BbsController extends Controller
 {
@@ -12,5 +13,9 @@ class BbsController extends Controller
     public function create(Request $request){
         echo "bbs/index.blade.phpのinput(name='hoge')に入力された値は $request->hoge です<br>";
         echo "bbs/index.blade.phpのinput(name='body')に入力された値は $request->body です";
+        $post = new Post;
+        $post->title = $request->hoge;
+        $post->body = $request->body;
+        $post->save();
     }
 }
