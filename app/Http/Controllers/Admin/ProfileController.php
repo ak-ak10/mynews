875a0158bfa1c2591ee944ab88926e42b2684a60
名前 <>
2021-09-09 01:50:19 +0000

    laravel14 kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class profileController extends Controller
 {
@@ -13,8 +14,14 @@ class profileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $profile = new Profile;
+        $profile->name = $request->hoge;
+        $profile->gender = $request->gender;
+        $profile->hobby = $request->hobby;
+        $profile->introduction = $request->introduction;
+        $profile->save();
         return redirect('admin/profile/create');
     }
 
